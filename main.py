ffc175f0cabf550d7bd5bae5e979f3664656f4af
Sameer <sameer.subhedar99@gmail.com>
2019-05-28 01:32:35 +0530

    Made necessary changes

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame,sys
+
+pygame.init()
+
+size = w,h = 1000,1000  # Screen size
+
+scr = pygame.display.set_mode(size)
+
+list_of_players = ["p1","p2","p3","p4","p5","p6","p7","p8"]
+
+# TBC 
